cfd64990c07ebb90be1cbc7e9cfc0922cfd57b6c
USER <USER@JHYOON-PC>
2023-07-25 16:50:31 +0900

    초기 커밋.

---
+++ Application/MainApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NoRoot.App.Application
+{
+    public class MainApp
+    {
+        public MainApp()
+        {
+
+        }
+
+        public void Init()
+        {
+            Console.WriteLine("Start NoRoot Application ...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NoRoot.App.Application;
+
+namespace NoRoot.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var main = new MainApp();
+            main.Init();
+        }
+    }
+}
